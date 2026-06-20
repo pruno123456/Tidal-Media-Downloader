aad94a6d04737fdfb455c26b6970b2a7b243f425
pruno123456 <oli.rawlings@yahoo.com>
2026-06-20 16:46:05 +0100

    Update setup.py
    pruno variant

--- TIDALDL-PY/setup.py
+++ TIDALDL-PY/setup.py
@@ -2,22 +2,28 @@ from setuptools import setup, find_packages
 from tidal_dl.printf import VERSION
 
 setup(
-    name='tidal-dl',
+    name='pruno-dl',
     version=VERSION,
     license="Apache2",
-    description="Tidal Music Downloader.",
+    description="Pruno custom TIDAL downloader.",
 
-    author='YaronH',
-    author_email="yaronhuang@foxmail.com",
+    author='Oli',
+    author_email="",
 
     packages=find_packages(exclude=['tidal_gui*']),
     include_package_data=False,
     platforms="any",
-    install_requires=["aigpy>=2022.7.8.1", 
-                      "requests>=2.22.0",
-                      "pycryptodome", 
-                      "pydub", 
-                      "prettytable",
-                      "lxml"],
-    entry_points={'console_scripts': ['tidal-dl = tidal_dl:main', ]}
+    install_requires=[
+        "aigpy>=2022.7.8.1",
+        "requests>=2.22.0",
+        "pycryptodome",
+        "pydub",
+        "prettytable",
+        "lxml",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pruno-dl = tidal_dl:main',
+        ]
+    }
 )
