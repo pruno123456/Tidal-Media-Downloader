e769aae6e7416cf3c81073177cc7d6589dec080e
pruno123456 <oli.rawlings@yahoo.com>
2026-06-20 18:00:22 +0100

    Update setup.py
    testing

--- TIDALDL-PY/setup.py
+++ TIDALDL-PY/setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
-from tidal_dl.printf import VERSION
+
+VERSION = "0.1.0"
 
 setup(
     name='pruno-dl',
     version=VERSION,
     license="Apache2",
-    description="Pruno custom TIDAL downloader.",
+    description="Custom TIDAL downloader for tagged library workflows.",
 
-    author='Oli',
-    author_email="",
+    author='YaronH',
+    author_email="yaronhuang@foxmail.com",
 
     packages=find_packages(exclude=['tidal_gui*']),
     include_package_data=False,
